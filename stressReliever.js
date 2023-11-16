e74d5baf5f513b4b294a10d4220426637b3c2550
CoreyC315 <122757429+CoreyC315@users.noreply.github.com>
2023-11-16 12:10:41 -0500

    Update stressReliever.js

--- stressReliever.js
+++ stressReliever.js
@@ -18,11 +18,13 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', readHelloMessage);
-router.get('/players', readPlayers);
-router.get('/players/:id', readPlayer);
-router.put('/players/:id', updatePlayer);
-router.post('/players', createPlayer);
-router.delete('/players/:id', deletePlayer);
+router.get('/users', readUsers);
+router.get('/fidget_toys', readFidgetToys);
+router.get('/user_activity', readUserActivity);
+router.get('/user_activity/:id', readUserActivityById);
+router.post('/user_activity', createUserActivity);
+router.put('/user_activity/:id', updateUserActivity);
+router.delete('/user_activity/:id', deleteUserActivity);
 
 app.use(router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -41,8 +43,8 @@ function readHelloMessage(req, res) {
   res.send('Hello, CS 262 Monopoly service!');
 }
 
-function readPlayers(req, res, next) {
-  db.many('SELECT * FROM Player')
+function readUsers(req, res, next) {
+  db.many('SELECT * FROM users')
     .then((data) => {
       res.send(data);
     })
@@ -51,28 +53,8 @@ function readPlayers(req, res, next) {
     });
 }
 
-function readPlayer(req, res, next) {
-  db.oneOrNone('SELECT * FROM Player WHERE id=${id}', req.params)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
-
-function updatePlayer(req, res, next) {
-  db.oneOrNone('UPDATE Player SET email=${body.email}, name=${body.name} WHERE id=${params.id} RETURNING id', req)
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
-
-function createPlayer(req, res, next) {
-  db.one('INSERT INTO Player(email, name) VALUES (${email}, ${name}) RETURNING id', req.body)
+function readFidgetToys(req, res, next) {
+  db.many('SELECT * FROM fidget_toys')
     .then((data) => {
       res.send(data);
     })
@@ -81,8 +63,48 @@ function createPlayer(req, res, next) {
     });
 }
 
-function deletePlayer(req, res, next) {
-  db.oneOrNone('DELETE FROM Player WHERE id=${id} RETURNING id', req.params)
+function readUserActivity(req, res, next) {
+  db.many('SELECT * FROM user_activity')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function readUserActivityById(req, res, next) {
+  db.oneOrNone('SELECT * FROM user_activity WHERE activity_id=${id}', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function createUserActivity(req, res, next) {
+  db.one('INSERT INTO user_activity(user_id, toy_id, start_time, end_time) VALUES (${user_id}, ${toy_id}, ${start_time}, ${end_time}) RETURNING activity_id', req.body)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function updateUserActivity(req, res, next) {
+  db.oneOrNone('UPDATE user_activity SET user_id=${user_id}, toy_id=${toy_id}, start_time=${start_time}, end_time=${end_time} WHERE activity_id=${params.id} RETURNING activity_id', req)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+function deleteUserActivity(req, res, next) {
+  db.oneOrNone('DELETE FROM user_activity WHERE activity_id=${id} RETURNING activity_id', req.params)
     .then((data) => {
       returnDataOr404(res, data);
     })
